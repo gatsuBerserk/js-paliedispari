1a737660f481725d3e1b79ee3ff3c09f99ecd42e
gatsuBerserk <berserkdev@outlook.it>
2022-03-03 16:33:26 +0100

    exercise 1

--- js/script.js
+++ js/script.js
@@ -4,12 +4,33 @@
         Creare una funzione per capire se la parola inserita è palindroma
 */ 
     // Chiedere all'utente di inserire una parola
-    const parola= prompt("inserisci una parola");
-    console.log(parola);
+    const parolaUtente= prompt("inserisci una parola");
+    console.log(parolaUtente);
     
     // Creare una funzione per capire se la parola inserita è palindroma
-   
+        
+    // Utilizziamo la risoluzione con true o false 
+        function isPalindrome (parola){
+            // Potremmo aiutarci con gli array e usare "Array.from()" in quanto ci restituisce un Array iterabile (Fonte MDN) 
+                const parolaArray= Array.from(parola); 
+                
+            // Utiliziamo un'altro metodo degli array  ".reverse" in qunato ci restituisce un array con gli elementi in ordine invertito (Fonte MDN)
+                const parolaReverse= parolaArray.reverse();
+            
+            // Utilizziamo un'altro metodo degli array join() in modo da concatenare tutti gli elementi dell'array (Fonte MDN)    
+                const parolaUnita = parolaReverse.join();
+            // Creiamo la condizione di verifica
+                if (parola === parolaReverse){
+                    console.log("è palindroma")
+                    return true;
+                }else{
+                    console.log("non è palindroma")
+                    return false;
+                   
+                }
+        };
 
+        console.log(isPalindrome(parolaUtente));
 
 
 
